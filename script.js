818ae715b6b23bcec65f404e8f9da43cd87122b2
DanielFerreira <daniel18mtm@gmail.com>
2023-07-06 17:16:04 -0300

    Exercício feito

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
+function verificaVazio() {
+  const value1 = document.getElementById('value1').value;
+  const value2 = document.getElementById('value2').value;
+  if (value1 === '' || value2 === '') {
+    throw new Error('Preencha os campos para realizar a soma');
+  }
+}
+
+function verificaNumerico() {
+  const value1 = document.getElementById('value1').value;
+  const value2 = document.getElementById('value2').value;
+  const number1 = Number(value1);
+  const number2 = Number(value2);
+  console.log(typeof number1, typeof number2);
+  if (Number.isNaN(number1) || Number.isNaN(number2)) {
+    throw new Error('Informe dois números para realizar a soma');
+  }
+}
+
 function calculateSum() {
   const value1 = document.getElementById('value1').value;
   const value2 = document.getElementById('value2').value;
-  const result = Number(value1) + Number(value2);
 
-  return result;
+  const value3 = document.getElementById('value1');
+
+  try {
+    verificaVazio();
+    verificaNumerico();
+    return Number(value1) + Number(value2);
+  } catch (error) {
+    return error.message;
+  } finally {
+    value3.value = '';
+    document.getElementById('value2').value = '';
+  }
 }
 
 function displayResult(result) {
